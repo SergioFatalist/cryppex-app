0dd5fddcf67728e2c11fe2ece49be358de094686
Serhii Mykhailovskyi <sergio@smounters.com>
2024-06-29 15:24:48 -0500

    wip

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,19 +1,87 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  devtools: { enabled: true },
-
+  modules: [
+    "nitro-cloudflare-dev",
+    "@pinia/nuxt",
+    "@pinia-plugin-persistedstate/nuxt",
+    "@nuxtjs/i18n",
+    "vuetify-nuxt-module",
+  ],
   nitro: {
     preset: "cloudflare-pages",
     prerender: {
-      autoSubfolderIndex: false
-    }
+      autoSubfolderIndex: false,
+    },
+  },
+  ssr: false,
+  build: {
+    transpile: ["vuetify"],
+  },
+  i18n: {
+    lazy: true,
+    strategy: "no_prefix",
+    dynamicRouteParams: false,
+    skipSettingLocaleOnNavigate: true,
+    langDir: "./i18n",
+    locales: [
+      {
+        code: "en-US",
+        iso: "en-US",
+        file: "en-US.json",
+        name: "English",
+      },
+      {
+        code: "ru-RU",
+        iso: "ru-RU",
+        file: "ru-RU.json",
+        name: "Russian",
+      },
+    ],
+    detectBrowserLanguage: {
+      useCookie: false,
+      fallbackLocale: "en",
+    },
+    vueI18n: "./i18n.config.ts",
+  },
+  vuetify: {
+    vuetifyOptions: {
+      icons: {
+        defaultSet: "mdi",
+      },
+      theme: {
+        themes: {
+          dark: {
+            dark: true,
+            colors: {
+              background: "#232b43",
+              // 'background': '#0C0F16',
+              surface: "#161A2E",
+              "surface-bright": "#3D52A0",
+              "surface-light": "#232b43",
+              // 'surface-variant': '#0C0F16',
+              "on-surface-variant": "#EEEEEE",
+              primary: "#3366FF",
+              secondary: "#254EDB",
+              error: "#FF652D",
+              info: "#3EB8F9",
+              success: "#87E21F",
+              warning: "#FFE121",
+            },
+          },
+        },
+        defaultTheme: "dark",
+      },
+    },
   },
 
-  modules: [
-    "nitro-cloudflare-dev",
-    "@nuxtjs/tailwindcss",
-    "@nuxtjs/google-fonts",
-    "nuxt-icon"
-  ],
-
-})
+  app: {
+    head: {
+      script: [{ src: "https://telegram.org/js/telegram-web-app.js" }],
+    },
+  },
+  devtools: { enabled: true },
+  devServer: {
+    host: "0.0.0.0",
+    port: 4200,
+  },
+});
---
+++ pages/index.vue
@@ -0,0 +1,12 @@
+<template>
+  <div>sample</div>
+  {{ app.isReady }}
+</template>
+
+<script setup lang="ts">
+import { useWebApp } from "vue-tg";
+
+const app = useWebApp();
+</script>
+
+<style scoped></style>
