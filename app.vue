168c0ce4e07761bcc3506d0f0bc610175bb1e7a8
WSRoot User <root@ws.utgid.com>
2024-07-02 03:35:21 +0000

    check platform

--- app.vue
+++ app.vue
@@ -3,3 +3,14 @@
     <NuxtPage />
   </NuxtLayout>
 </template>
+
+<script lang="ts" setup>
+const platform = Telegram.WebApp.platform;
+const router = useRouter();
+
+if (platform == "tdesktop") {
+  await router.push("/use-mobile");
+} else if (platform != "ios" && platform != "android") {
+  navigateTo("https://t.me/cryppex_dev_bot/cryppex_dev_app", { external: true });
+}
+</script>
--- pages/index.vue
+++ pages/index.vue
@@ -1,18 +1,20 @@
 <template>
   <div class="text-white">{{ h }}</div>
+  <div class="text-white">{{ r }}</div>
 </template>
 
 <script setup lang="ts">
-import { useWebApp } from "vue-tg";
-const app = useWebApp();
 const config = useRuntimeConfig();
+const router = useRouter();
 const h = ref(Telegram.WebApp.platform);
-
+const r = ref("");
 const send = async () => {
   const data = await $fetch("/api/validate", {
     method: "POST",
-    body: app.initData,
+    body: Telegram.WebApp.initData,
   });
+
+  r.value = data.result.toString();
 
   // if (error) {
   //   console.dir(error);
