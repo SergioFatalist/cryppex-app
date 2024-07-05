a28f91e04240853432edbd80f38706d846632058
Serhii Mykhailovskyi <sergio@smounters.com>
2024-07-05 11:08:58 -0500

    output dir

--- composables/formatTgName.ts
+++ composables/formatTgName.ts
@@ -4,5 +4,5 @@ export default function (user: User | undefined) {
   if (!user) {
     return "No DATA";
   }
-  return user.username ? `@${user.username}` : `${user.firstName} ${user.lastName}`;
+  return user.username ? `@${user.username}` : `${user.firstName ?? ""} ${user.lastName ?? ""}`;
 }
