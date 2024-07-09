0711642ab29ad4215cff83845015fa9f8169381b
Serhii Mykhailovskyi <sergio@smounters.com>
2024-07-09 18:09:36 -0500

    prisma

--- app.vue
+++ app.vue
@@ -30,16 +30,17 @@ const route = useRoute();
 const $app = useAppStore();
 
 const { $telegram } = useNuxtApp();
+const config = useRuntimeConfig();
 
 const ref = route.query["ref"];
 const kentId = ref ? (Array.isArray(ref) ? ref[0] || "" : ref).toString() : "";
 
-// const platform = $telegram.WebApp.platform;
-// if (platform == "tdesktop") {
-//   await router.push("/use-mobile");
-// } else if (platform != "ios" && platform != "android") {
-//   navigateTo("https://t.me/cryppex_dev_bot/cryppex_dev_app", { external: true });
-// }
+const platform = $telegram.WebApp.platform;
+if (platform == "tdesktop") {
+  await router.push("/use-mobile");
+} else if (platform != "ios" && platform != "android") {
+  navigateTo(config.public.appUrl, { external: true });
+}
 
 // const potap =
 //   "user=%7B%22id%22%3A5288550634%2C%22first_name%22%3A%22Potap%22%2C%22last_name%22%3A%22%22%2C%22username%22%3A%22SergioPotap%22%2C%22language_code%22%3A%22ru%22%2C%22allows_write_to_pm%22%3Atrue%7D&chat_instance=-1260992627038743562&chat_type=private&auth_date=1719972346&hash=8b3b8b12361475d3951de90d327a341b19c0368d912fcdd85c5c75ef0474035d";
@@ -49,7 +50,7 @@ const setUser = async () => {
   const initData = $telegram.WebApp.initData;
   // const initData = potap;
 
-  if (!initData) {
+  if (!initData || initData.length < 10) {
     await router.push("/no-data");
     return;
   }
