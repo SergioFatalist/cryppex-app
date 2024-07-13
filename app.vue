fcd06a3c62f58aa8a0bee511174eb9389b071a44
Serhii Mykhailovskyi <sergio@smounters.com>
2024-07-12 21:46:54 -0500

    prerelease?

--- app.vue
+++ app.vue
@@ -3,7 +3,7 @@
     <v-main>
       <NuxtPage />
     </v-main>
-    <v-bottom-navigation>
+    <v-bottom-navigation v-if="mobile">
       <v-btn to="/">
         <v-icon>mdi-home-outline</v-icon>
         <span>Home</span>
@@ -32,12 +32,14 @@ const $app = useAppStore();
 const $config = useRuntimeConfig();
 const { $telegram } = useNuxtApp();
 
-// const platform = $telegram.WebApp.platform;
-// if (platform == "tdesktop") {
-//   await router.push("/use-mobile");
-// } else if (platform != "ios" && platform != "android") {
-//   navigateTo($config.public.appUrl, { external: true });
-// }
+const platform = $telegram.WebApp.platform;
+const mobile = ref<boolean>(platform == "ios" || platform == "android");
+
+if (platform !== "unknown" && !mobile.value) {
+  await router.push("/use-mobile");
+} else if (!mobile.value) {
+  navigateTo($config.public.appUrl, { external: true });
+}
 
 // const potap =
 //   "user=%7B%22id%22%3A5288550634%2C%22first_name%22%3A%22Potap%22%2C%22last_name%22%3A%22%22%2C%22username%22%3A%22SergioPotap%22%2C%22language_code%22%3A%22ru%22%2C%22allows_write_to_pm%22%3Atrue%7D&chat_instance=-1260992627038743562&chat_type=private&auth_date=1719972346&hash=8b3b8b12361475d3951de90d327a341b19c0368d912fcdd85c5c75ef0474035d";
