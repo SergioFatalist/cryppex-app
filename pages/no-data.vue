7ca7bc8047f76ebfdd1c3e4960af3f287ba0c626
Serhii Mykhailovskyi <sergio@smounters.com>
2024-07-13 09:35:37 -0500

    prerelease?

--- pages/no-data.vue
+++ pages/no-data.vue
@@ -4,7 +4,6 @@
       <div class="d-flex flex-column justify-space-between h-100">
         <v-spacer />
         <div class="text-white text-center text-h5">No Required DATA<br />Please close and open Application again</div>
-        {{ app.user }}
         <v-spacer />
       </div>
     </div>
